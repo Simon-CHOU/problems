f0401755fd68654e476b811e422ce70dabd5c047
Simon-Chou <mrsimonzhou@gmail.com>
2019-03-17 11:09:13 +0800

    first commit

---
+++ src/components/Table.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="日日期" width="180"></el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+    <el-table-column prop="address" label="地址"></el-table-column>
+  </el-table>
+
+  <!-- <div>
+        <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+        <h1>我是表格牛阿</h1>
+  </div>-->
+</template>
+ <script>
+import axios from 'axios';
+export default {
+  create() {
+    axios
+      .get("http://127.0.0.1:8082/problem/list")
+      .then(response => {
+        console.log(response);
+        console.log("heeeeeeellllooooo");
+        this.problem = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        alert("网络错误，不能访问");
+      });
+      axios.get('https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+    // https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
+    //http://127.0.0.1:8082/problem/list"
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
+      problem: [{}]
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import {Table,TableColumn,} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+Vue.use(Table);
+Vue.use(TableColumn);
 
 Vue.config.productionTip = false
 
